bf3536f2b530b52ef008642891ca810a3b033260
KoorunG <onlyzion10@naver.com>
2022-04-05 23:20:44 +0900

    modify project

---
+++ src/main/java/com/koorung/book/freelecspringbootaws/FreelecSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.koorung.book.freelecspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koorung/book/freelecspringbootaws/FreelecSpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koorung.book.freelecspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
